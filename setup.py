e2c8f26f9d9a4419f785c9a39062c6700b8d7324
ToranAustrene <59725388+ToranAustrene@users.noreply.github.com>
2022-10-04 11:27:37 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='SpreadsheetUpdater',
+    version='1',
+    # packages=[
+    #     'json',
+    #     'requests',
+    #     'cbpro',
+    #     'gspread',
+    #     'time'
+    #     ],
+    install_requires=[
+        'json',
+        'requests',
+        'cbpro',
+        'gspread',
+        'time'
+        ],
+    url='',
+    license='',
+    author='CatatonicMink',
+    author_email='',
+    description=''
+    )
